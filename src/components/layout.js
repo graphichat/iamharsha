fb3710d8e4d96a763b757b6527b175dbada9baf2
Praveenkumar <praveen.nalakurthi@gmail.com>
2021-02-12 10:04:07 +0530

    initial Build

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+// import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      {/* <Header siteTitle={data.site.siteMetadata?.title || `Title`} /> */}
       <div
         style={{
           margin: `0 auto`,
@@ -39,9 +39,9 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
+          {/* © {new Date().getFullYear()}, Built with */}
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          {/* <a href="https://www.gatsbyjs.com">Gatsby</a> */}
         </footer>
       </div>
     </>
